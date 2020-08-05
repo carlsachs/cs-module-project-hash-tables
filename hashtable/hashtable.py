7335f7268c8281141fe937ab64c5818dceae0fa2
carlsachs <sachscarl@gmail.com>
2020-08-05 11:23:02 -0400

    day 2 MVP done

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,22 +2,30 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
+
+
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
+
+
 class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
     Implement this.
     """
+
     def __init__(self, capacity):
-        # Your code here
-        self.capacity = capacity if capacity >= MIN_CAPACITY else MIN_CAPACITY
-        self.list = [None] * self.capacity
+        self.capacity = capacity
+        self.size = 0
+        self.keys = []
+        self.storage = [None] * capacity if capacity >= MIN_CAPACITY else [None] * MIN_CAPACITY
+
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
@@ -26,35 +34,36 @@ class HashTable:
         One of the tests relies on this.
         Implement this.
         """
-        # Your code here
+        return len(self.storage)
+
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
         Implement this.
         """
-        # Your code here
+        return self.size / self.capacity
+
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
         Implement this, and/or DJB2.
         """
-        # Your code here
-        hash_val = 14695981039346656037
-        for char in key:
-            hash_val = hash_val * 1099511628211
-            hash_val = hash_val ^ ord(char)
-        return hash_val
+        hash = 0xcbf29ce484222325
+        for character in key:
+            hash ^= ord(character)
+            hash *= 0x100000001b3
+            hash &= 0xffffffffffffffff
+        return hash
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
         Implement this, and/or FNV-1.
         """
-        # Your code here
-        hash_val = 5381
-        for char in key:
-            hash_val = ((hash_val << 5) + hash_val) + ord(char)
-        return hash_val & 0xFFFFFFFF
+        hash = 5381
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
     def hash_index(self, key):
         """
@@ -70,8 +79,26 @@ class HashTable:
         Hash collisions should be handled with Linked List Chaining.
         Implement this.
         """
-        # Your code here
-        self.list[self.hash_index(key)] = value
+        self.size += 1
+        self.keys.append(key)
+        index = self.hash_index(key)
+        node = self.storage[index]
+        if node is None:
+            self.storage[index] = HashTableEntry(key, value)
+            return
+        prev = node
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+        if node != None:
+            node.value = value
+        prev.next = HashTableEntry(key, value)
+        # resize if load factor is too large
+        load = self.get_load_factor()
+        if load > 0.7:
+            self.resize(self.capacity * 2)
+        if load < 0.2:
+            self.resize(self.capacity / 2)
 
     def delete(self, key):
         """
@@ -79,11 +106,23 @@ class HashTable:
         Print a warning if the key is not found.
         Implement this.
         """
-        # Your code here
-        if(self.list[self.hash_index(key)] == None):
-            print("This Key Does Not Exist")
-            return
-        self.list[self.hash_index(key)] = None
+        index = self.hash_index(key)
+        node = self.storage[index]
+        prev = None
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+        if node is None:
+            return None
+        else:
+            self.size -= 1
+            self.keys.remove(key)
+            result = node.value
+            if prev is None:
+                self.storage[index] = node.next
+            else:
+                prev.next = prev.next.next
+            return result
 
     def get(self, key):
         """
@@ -91,42 +130,62 @@ class HashTable:
         Returns None if the key is not found.
         Implement this.
         """
-        # Your code here
-        if(self.list[self.hash_index(key)] == None):
+        index = self.hash_index(key)
+        node = self.storage[index]
+        while node is not None and node.key != key:
+            node = node.next
+        if node is None:
             return None
-        return self.list[self.hash_index(key)]
-        
+        else:
+            return node.value
+
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
         Implement this.
         """
-        # Your code here
-    if __name__ == "__main__":
-        ht = HashTable(8)
-        ht.put("line_1", "'Twas brillig, and the slithy toves")
-        ht.put("line_2", "Did gyre and gimble in the wabe:")
-        ht.put("line_3", "All mimsy were the borogoves,")
-        ht.put("line_4", "And the mome raths outgrabe.")
-        ht.put("line_5", '"Beware the Jabberwock, my son!')
-        ht.put("line_6", "The jaws that bite, the claws that catch!")
-        ht.put("line_7", "Beware the Jubjub bird, and shun")
-        ht.put("line_8", 'The frumious Bandersnatch!"')
-        ht.put("line_9", "He took his vorpal sword in hand;")
-        ht.put("line_10", "Long time the manxome foe he sought--")
-        ht.put("line_11", "So rested he by the Tumtum tree")
-        ht.put("line_12", "And stood awhile in thought.")
-        print("")
-        # Test storing beyond capacity
-        for i in range(1, 13):
-            print(ht.get(f"line_{i}"))
-        # Test resizing
-        old_capacity = ht.get_num_slots()
-        ht.resize(ht.capacity * 2)
-        new_capacity = ht.get_num_slots()
-        print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-        # Test if data intact after resizing
-        for i in range(1, 13):
-            print(ht.get(f"line_{i}"))
-        print("")
+        old_storage = {}
+        for i in range(len(self.keys)):
+            key = self.keys[i]
+            old_storage[key] = self.get(key)
+        self.capacity = new_capacity
+        self.storage = [None] * self.capacity
+        self.size = 0
+        for key, value in old_storage.items():
+            self.put(key, value)
+
+if __name__ == "__main__":
+    ht = HashTable(8)
+
+    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    ht.put("line_2", "Did gyre and gimble in the wabe:")
+    ht.put("line_3", "All mimsy were the borogoves,")
+    ht.put("line_4", "And the mome raths outgrabe.")
+    ht.put("line_5", '"Beware the Jabberwock, my son!')
+    ht.put("line_6", "The jaws that bite, the claws that catch!")
+    ht.put("line_7", "Beware the Jubjub bird, and shun")
+    ht.put("line_8", 'The frumious Bandersnatch!"')
+    ht.put("line_9", "He took his vorpal sword in hand;")
+    ht.put("line_10", "Long time the manxome foe he sought--")
+    ht.put("line_11", "So rested he by the Tumtum tree")
+    ht.put("line_12", "And stood awhile in thought.")
+
+    print("")
+
+    # Test storing beyond capacity
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    # Test resizing
+    old_capacity = ht.get_num_slots()
+    ht.resize(ht.capacity * 2)
+    new_capacity = ht.get_num_slots()
+
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    # Test if data intact after resizing
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    print("")
---
+++ hashtable/python.py
@@ -0,0 +1,10 @@
+# Print out all of the strings in the following array in alphabetical order, each on a separate line.
+
+
+names = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+
+sorted_list = sorted(names)
+print(sorted_list)
+
+for l in sorted_list:
+    print(l)
