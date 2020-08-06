e76e0ef1c78236ff5bd6bcab44cfe2ec6c273f9d
carlsachs <sachscarl@gmail.com>
2020-08-06 12:46:11 -0400

    finished lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,8 @@
 # Your code here
 
+import random
+import math
+
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -15,8 +18,12 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    cache = {}
 
+    if (x, y) not in cache:
+        cache[(x, y)] = slowfun_too_slow(x, y)
 
+    return cache[(x, y)]
 
 # Do not modify below this line!
 
