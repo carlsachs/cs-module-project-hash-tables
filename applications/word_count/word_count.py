fbfa67f412ee8431399beaddc6c6b6bb12709ba8
carlsachs <sachscarl@gmail.com>
2020-08-07 12:16:02 -0400

    MVP Push

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,24 @@
 def word_count(s):
     # Your code here
+    d = dict()
+
+    char_list = '"":;.,-+=/\|[]{}()*^&'
+
+    new_s = s.lower().replace("\t", " ").replace(
+        "\n", " ").replace("\r", " ").split(" ")
+
+    print(new_s)
+
+    for word in new_s:
+        new_word = word.strip(char_list)
+        print(new_word)
+
+        if new_word not in d and new_word != "":
+            d[new_word] = 1
+        elif new_word != "":
+            d[new_word] += 1
+
+    return d
 
 
 
